d6afc0e9fa9259c626f45a127abf2ed26dd20486
ainur <ainurkhaprianto@gmail.com>
2022-03-04 08:49:42 +0700

    new

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Configuration(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:admin''@localhost:5432/testflask'
+    # SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:admin''@localhost:5432/testflask'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
